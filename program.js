e97a9250747a51af7ebcd9c38387c94f1e266341
Hussein A <ha.246@outlook.com>
2024-11-25 00:18:11 -0600

    added deposit and line check system

--- program.js
+++ program.js
@@ -1,3 +1,32 @@
-// 1. Deposit Money
-// 2. Collect a bet amount
-// test
+const prompt = require('prompt-sync')();
+
+const deposit = () => {
+    while (true) {
+        const depositAmount = prompt('Enter amount to deposit: ');
+        const numberDepositAmount = parseFloat(depositAmount);
+
+        if (isNaN(numberDepositAmount) || numberDepositAmount <= 0) {
+            console.log("Please enter a valid number");
+    } else {
+        return numberDepositAmount;
+        }
+    }
+};
+
+const getNumberOfLines = () => {
+    while (true) {
+        const lines = prompt('Enter amount of lines: ');
+        const numberOfLines = parseFloat(lines);
+
+        if (isNaN(numberOfLines) || numberOfLines <= 0 || numberOfLines < 4) {
+            console.log("Please enter a valid number");
+    } else {
+        return numberOfLines;
+        }
+    }
+};
+
+//const amountDeposit = deposit();
+const lineAmount = getNumberOfLines();
+//console.log(amountDeposit);
+console.log(lineAmount);

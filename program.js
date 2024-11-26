6cc81b7ada65c7b664ed86899ea1df52fa9c6e6a
Hussein A <ha.246@outlook.com>
2024-11-26 15:50:35 -0600

    quick update to fix a few bugs

--- program.js
+++ program.js
@@ -7,26 +7,38 @@ const deposit = () => {
 
         if (isNaN(numberDepositAmount) || numberDepositAmount <= 0) {
             console.log("Please enter a valid number");
-    } else {
-        return numberDepositAmount;
+        } else {
+            return numberDepositAmount;
         }
     }
 };
 
 const getNumberOfLines = () => {
     while (true) {
-        const lines = prompt('Enter amount of lines: ');
+        const lines = prompt('Enter amount of lines (1-3): ');
         const numberOfLines = parseFloat(lines);
 
-        if (isNaN(numberOfLines) || numberOfLines <= 0 || numberOfLines < 4) {
+        if (isNaN(numberOfLines) || numberOfLines <= 0 || numberOfLines > 3) {
             console.log("Please enter a valid number");
-    } else {
-        return numberOfLines;
+        } else {
+            return numberOfLines;
         }
     }
 };
 
-//const amountDeposit = deposit();
+const getBet = (balance, lines) => {
+    while (true) {
+        const stringBet = prompt('Enter bet amount per line: ');
+        const bet = parseFloat(stringBet);
+
+        if (isNaN(bet) || bet <= 0 || bet > balance / lines) {
+            console.log("Invalid bet amount");
+        } else {
+            return bet;
+        }
+    }
+};
+
+let balance = deposit();
 const lineAmount = getNumberOfLines();
-//console.log(amountDeposit);
-console.log(lineAmount);
+const bet = getBet(balance, lineAmount);

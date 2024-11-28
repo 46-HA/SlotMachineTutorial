9f7468fefe909829e85355e4ffdf565a5d2844f7
Hussein A <ha.246@outlook.com>
2024-11-27 21:37:49 -0600

    Adding final touches

---
+++ README.md
@@ -0,0 +1,11 @@
+# Slot Machine Tutorial
+
+This project is a simple slot machine game created by following a tutorial.
+
+## Tutorial
+
+You can find the tutorial [here](https://www.youtube.com/watch?v=E3XxeE7NF30&t=1349s&ab_channel=TechWithTim).
+
+## Description
+
+This is a quick project I worked on before jumping into React
--- program.js
+++ program.js
@@ -1,5 +1,36 @@
 const prompt = require('prompt-sync')();
 
+const ROWS = 3;
+const COLUMNS = 3;
+
+const SYMBOL_COUNT = {
+    "A": 2,
+    "B": 4,
+    "C": 6,
+    "D": 8
+}
+
+const SYMBOL_VALUES = {
+    "A": 5,
+    "B": 4,
+    "C": 3,
+    "D": 2
+};
+
+// Function to calculate winnings
+const calculateWinnings = (reels, bet, lines) => {
+    let winnings = 0;
+    for (let i = 0; i < lines; i++) {
+        const symbols = reels.map(reel => reel[i]);
+        const allSame = symbols.every(symbol => symbol === symbols[0]);
+        if (allSame) {
+            winnings += bet * SYMBOL_VALUES[symbols[0]];
+        }
+    }
+    return winnings;
+};
+
+// Deposit function
 const deposit = () => {
     while (true) {
         const depositAmount = prompt('Enter amount to deposit: ');
@@ -13,6 +44,7 @@ const deposit = () => {
     }
 };
 
+// Get the number of lines the user wants to bet on (1-3)
 const getNumberOfLines = () => {
     while (true) {
         const lines = prompt('Enter amount of lines (1-3): ');
@@ -26,6 +58,7 @@ const getNumberOfLines = () => {
     }
 };
 
+// Get the bet amount for each line
 const getBet = (balance, lines) => {
     while (true) {
         const stringBet = prompt('Enter bet amount per line: ');
@@ -39,6 +72,36 @@ const getBet = (balance, lines) => {
     }
 };
 
+// Spin the reels and return the results
+const spin = () => {
+    const symbols = [];
+    for (const [symbol, count] of Object.entries(SYMBOL_COUNT)) {
+        for (let i = 0; i < count; i++) {
+            symbols.push(symbol);
+        }
+    }
+
+    const REELS = [[], [], []];
+    for (let i = 0; i < COLUMNS; i++) {
+        const reelSymbols = [...symbols];
+        for (let j = 0; j < ROWS; j++) {
+            const randomIndex = Math.floor(Math.random() * reelSymbols.length);
+            const selectSymbol = reelSymbols[randomIndex];
+            REELS[i].push(selectSymbol);
+            reelSymbols.splice(randomIndex, 1);
+        }
+    }
+    return REELS;
+};
+
+// Main game flow
 let balance = deposit();
 const lineAmount = getNumberOfLines();
 const bet = getBet(balance, lineAmount);
+
+const reels = spin();
+console.log('Reels:', reels);
+
+const winnings = calculateWinnings(reels, bet, lineAmount);
+balance += winnings;
+console.log("You won, your new balance is: $" + balance);
